0a9bea7b7f77abed935831a202aca2c6250ccda3
Christophe Lalevee <lalevee@google.com>
2020-05-14 23:29:24 +0200

    test

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const core = require('@actions/core')
 const newman = require('newman')
 
-// call newman.run to pass `options` object and wait for callback
-newman.run({
-    collection: require('./petstore.postman_collection.json'),
-    reporters: 'cli'
-}, function (err) {
-	if (err) { throw err; }
-    console.log('collection run complete!');
-});
+try {
+  const get = core.getInput
+  const apiBase = 'https://api.getpostman.com'
+  const idRegex = /^[0-9]{7}-\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/
+
+  const options = {
+    apiKey: '?apikey=' + get('postmanApiKey'),
+    collection: get('collection'),
+    environment: get('environment'),
+    globals: get('globals'),
+    iterationCount: Number(get('iterationCount')),
+    iterationData: get('iterationData'),
+    folder: get('folder').split(','),
+    workingDir: get('workingDir'),
+    insecureFileRead: JSON.parse(get('insecureFileRead')),
+    timeout: Number(get('timeout')),
+    timeoutRequest: Number(get('timeoutRequest')),
+    timeoutScript: Number(get('timeoutScript')),
+    delayRequest: Number(get('delayRequest')),
+    ignoreRedirects: JSON.parse(get('ignoreRedirects')),
+    insecure: JSON.parse(get('insecure')),
+    bail: JSON.parse(get('bail')),
+    suppressExitCode: JSON.parse(get('suppressExitCode')),
+    reporters: get('reporters').split(','),
+    reporter: JSON.parse(get('reporter') || null),
+    color: get('color'),
+    sslClientCert: get('sslClientCert'),
+    sslClientKey: get('sslClientKey'),
+    sslClientPassphrase: get('sslClientPassphrase')
+  }
+  // call newman.run to pass `options` object and wait for callback
+  newman.run(options, function (err) {
+    if (err) { throw err; }
+      console.log('collection run complete!');
+  });
+} catch (error) {
+  core.setFailed(error.message)
+}
